570a3b666e9f9d6198eae500ab71ea349c2f6309
Hipoha <reda.mateusz@gmail.com>
2017-11-22 10:52:04 +0100

    8.2. try-catch-finally [theory]

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -1,8 +1,8 @@
 package com.kodilla.exception.io;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
@@ -10,11 +10,19 @@ public class FileReader {
 
     public void readFile() {
         ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("file/names.txt").getFile());
-        Path path = Paths.get(file.getPath());
-        Stream<String> fileLines = Files.lines(path);
 
-        System.out.println(file.getPath());
+        File file = new File(classLoader.getResource("file/names.txt").getFile());
+
+        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
+            fileLines.forEach(System.out::println);
+        } catch (IOException e) {
+            System.out.println("File not found! Error: " + e);
+        } finally {
+            System.out.println("This should be printed in any case.");
+        }
+
+        //System.out.println(file.getPath());
+
     }
 
 }
